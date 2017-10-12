e2402d60ee982bed0050b0b0c8a60dff53e8a75c
Austin Tackaberry <austintackaberry@gmail.com>
2017-10-11 21:59:00 -0700

    finished file size teller

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      fileName: " No file chosen",
+      file: '',
+      size: ''
+    }
+    this.handleBrowseClick = this.handleBrowseClick.bind(this);
+    this.handleChosenFile = this.handleChosenFile.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleBrowseClick() {
+    this.inputElement.click();
+  }
+
+  handleChosenFile() {
+    this.setState({fileName:this.inputElement.files[0].name});
+    this.setState({file: this.inputElement.files[0]});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    var sendFile = new FormData()
+    sendFile.append("file",this.state.file);
+
+    fetch('/upload', {
+      method: 'POST',
+      body: sendFile
+    }).then(
+      res => res.json()
+    ).then(data => {
+      if (data.size < 1000) {
+        this.setState({size: Math.round(data.size*10)/10 + " bytes"})
+      }
+      else if (data.size < 1000*1000) {
+        this.setState({size: Math.round(data.size/1000*10)/10 + " kB"})
+      }
+      else if (data.size < 1000*1000*1000) {
+        this.setState({size: Math.round(data.size/1000/1000*10)/10 + " MB"})
+      }
+      else if (data.size < 1000*1000*1000*1000) {
+        this.setState({size: Math.round(data.size/1000/1000/1000*10)/10 + " GB"})
+      }
+    }
+  )}
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">File Metadata Microservice</h1>
+          <p>Upload your sexy file right here</p>
+          <form onSubmit={this.handleSubmit}>
+            <input ref={input => this.inputElement = input} onChange={this.handleChosenFile} type="file" id="img-upload" />
+            <input type="button" id="browse" value="Choose File" onClick={this.handleBrowseClick} />
+            <p id="fileName">{this.state.fileName} </p>
+            <button id="submit" >Submit</button>
+          </form>
+          <p>{this.state.size}</p>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
--- react-backend/app.js
+++ react-backend/app.js
@@ -3,9 +3,16 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
+const fs = require("fs");
+var bb = require('express-busboy');
 
 var app = express();
+bb.extend(app, {
+  upload: true,
+  path: path.join(__dirname, 'public'),
+  allowedPath: /./
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -14,28 +21,34 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/images')));
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+//
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.post('/upload', function(req,res) {
+  const fileSize = req.files;
+  var stats = fs.statSync(req.files.file.file);
+  res.json(stats);
 });
 app.listen(3001);
+console.log("listening on port 3001");
 
 module.exports = app;
